07ace8137c3084c60366577cdc3bcb46f2f22e5a
peterdez <adebanjopete@gmail.com>
2023-11-24 00:08:01 +0100

    Clean up

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 
 //app.use(cors(corsOptions));
 
-app.use(cors());
+//app.use(cors());
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
