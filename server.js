ac41dcb11c973c4996ab9b069b3c0a33f21eeec0
peterdez <adebanjopete@gmail.com>
2023-11-23 22:37:13 +0100

    Handle cors error

--- server.js
+++ server.js
@@ -13,6 +13,20 @@ const app = express();
 
 app.use(cors());
 
+// Curb Cors Error by adding a header here
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 
